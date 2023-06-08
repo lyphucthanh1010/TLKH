70e683490c9ec6656c1984a11cc2353ec9b551ba
lyphucthanh1010 <phucthanhly1010@gmail.com>
2023-06-08 08:23:04 +0700

    first commit

---
+++ DashBoard.js
@@ -0,0 +1,43 @@
+const add = document.getElementById("add").addEventListener("click", function(event){
+    forgetpass()
+    event.preventDefault();
+  });
+  async function forgetpass(){
+    window.location.href ="Add.html"
+  }
+const history = document.getElementById("history").addEventListener("click", function(event){
+    window.location.href="History.html";
+    event.preventDefault();
+  });
+const calendar = document.getElementById("calendar").addEventListener("click", function(event){
+    window.location.href="Calendar.html";
+    event.preventDefault();
+  });
+const statistics = document.getElementById("statistics").addEventListener("click", function(event){
+    window.location.href="Statistics.html";
+    event.preventDefault();
+  });
+const community = document.getElementById("community").addEventListener("click", function(event){
+    window.location.href="Community.html";
+    event.preventDefault();
+  });
+const mywallet = document.getElementById("mywallet").addEventListener("click", function(event){
+    window.location.href="MyWallet.html";
+    event.preventDefault();
+  });
+const exchange = document.getElementById("exchange").addEventListener("click", function(event){
+    window.location.href="History.html";
+    event.preventDefault();
+  });
+const message = document.getElementById("message").addEventListener("click", function(event){
+    window.location.href="History.html";
+    event.preventDefault();
+  });
+const notification = document.getElementById("notification").addEventListener("click", function(event){
+    window.location.href="Noti.html";
+    event.preventDefault();
+  });
+const settings = document.getElementById("settings").addEventListener("click", function(event){
+    window.location.href="History.html";
+    event.preventDefault();
+  });
